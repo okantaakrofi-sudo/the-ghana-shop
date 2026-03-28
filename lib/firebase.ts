ff3ff2077f350b094d92e97e43f3a4b4f0b09df9
okantaakrofi-sudo <okantaakrofi@gmail.com>
2026-03-28 04:01:51 +0400

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAzgKJ8zeS7zGlznBrhNzBNf431V2-Wb6Q",
-  authDomain: "ghana-shop-e10a2.firebaseapp.com",
-  databaseURL: "https://ghana-shop-e10a2-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "ghana-shop-e10a2",
-  storageBucket: "ghana-shop-e10a2.firebasestorage.app",
-  messagingSenderId: "1039219520459",
-  appId: "1:1039219520459:web:3ef7ba837f419d1081fc4e",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
