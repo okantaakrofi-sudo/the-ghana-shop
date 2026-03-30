bc1f00bca037cbc64ab9753e52e8d6e7aed33697
okantaakrofi-sudo <okantaakrofi@gmail.com>
2026-03-30 18:39:28 +0400

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -8,9 +8,10 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID, // 👈 ADD THIS
 };
 
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 const db = getFirestore(app);
 
