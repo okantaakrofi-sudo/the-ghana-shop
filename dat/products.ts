1081fd40c892d209e33e7c792d4065008c2b3275
okantaakrofi-sudo <okantaakrofi@gmail.com>
2026-04-01 21:05:24 +0400

    Create products.ts

---
+++ dat/products.ts
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    id: 1,
+    name: "Mama Ghana Hair Oil",
+    price: 80,
+    image: "/oil.jpg",
+  },
+  {
+    id: 2,
+    name: "Shea Butter",
+    price: 50,
+    image: "/shea.jpg",
+  },
+  {
+    id: 3,
+    name: "African Black Soap",
+    price: 40,
+    image: "/soap.jpg",
+  },
+];
