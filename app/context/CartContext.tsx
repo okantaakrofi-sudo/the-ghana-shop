2c46c89f2621c3929c5cce162599895f5a5167ab
okantaakrofi-sudo <okantaakrofi@gmail.com>
2026-04-01 20:44:38 +0400

    Update CartContext.tsx

--- app/context/CartContext.tsx
+++ app/context/CartContext.tsx
@@ -4,20 +4,20 @@ import { createContext, useContext, useState } from "react";
 
 const CartContext = createContext<any>(null);
 
-export const CartProvider = ({ children }: any) => {
-  const [cart, setCart] = useState<any[]>([]);
+export function CartProvider({ children }: any) {
+  const [cart, setCart] = useState([]);
 
   const addToCart = (product: any) => {
-    setCart((prev) => [...prev, product]);
+    setCart((prev: any) => [...prev, product]);
   };
 
-  const total = cart.reduce((sum, item) => sum + item.price, 0);
+  const total = cart.reduce((sum: number, item: any) => sum + item.price, 0);
 
   return (
     <CartContext.Provider value={{ cart, addToCart, total }}>
       {children}
     </CartContext.Provider>
   );
-};
+}
 
 export const useCart = () => useContext(CartContext);
