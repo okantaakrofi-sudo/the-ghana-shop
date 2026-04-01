192b6109ba48bf7b0be62f08b24a1488f3246605
okantaakrofi-sudo <okantaakrofi@gmail.com>
2026-04-01 20:46:12 +0400

    Update products.ts

--- app/data/products.ts
+++ app/data/products.ts
@@ -1,20 +1,20 @@
 export const products = [
   {
     id: 1,
-    name: "Enhanced Hair Tea",
-    price: 45,
-    image: "/products/hair-tea.jpg",
+    name: "Mama Ghana Hair Oil",
+    price: 80,
+    image: "/oil.jpg",
   },
   {
     id: 2,
-    name: "Mama Ghana Hair Oil",
-    price: 60,
-    image: "/products/hair-oil.jpg",
+    name: "Shea Butter",
+    price: 50,
+    image: "/shea.jpg",
   },
   {
     id: 3,
-    name: "Yellow Shea Oil",
-    price: 50,
-    image: "/products/shea-oil.jpg",
+    name: "African Black Soap",
+    price: 40,
+    image: "/soap.jpg",
   },
 ];
