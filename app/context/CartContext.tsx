65a2b12c6ec41077f3fb76cd7049b33d47912d76
okantaakrofi-sudo <okantaakrofi@gmail.com>
2026-04-01 18:20:04 +0400

    Create CartContext.tsx

---
+++ app/context/CartContext.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+const CartContext = createContext<any>(null);
+
+export const CartProvider = ({ children }: any) => {
+  const [cart, setCart] = useState<any[]>([]);
+
+  const addToCart = (product: any) => {
+    setCart((prev) => [...prev, product]);
+  };
+
+  const total = cart.reduce((sum, item) => sum + item.price, 0);
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, total }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
