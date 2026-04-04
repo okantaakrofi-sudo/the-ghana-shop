c2b70975fa1f659d1532c79f454f36981355ea63
okantaakrofi-sudo <okantaakrofi@gmail.com>
2026-04-05 03:58:08 +0400

    Delete lib directory

--- lib/auth.ts
+++
@@ -1,8 +0,0 @@
-import { getAuth } from "firebase/auth";
-import { initializeApp } from "firebase/app";
-
-const app = initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-});
-
-export const auth = getAuth(app);
