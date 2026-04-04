05bda2aec1988fc63f9e10ff3464149052f5b026
okantaakrofi-sudo <okantaakrofi@gmail.com>
2026-04-04 09:30:05 +0400

    Delete app/api/orders directory

--- app/api/orders/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST(req: Request) {
-  const data = await req.json();
-
-  console.log("New Order:", data);
-
-  return NextResponse.json({ success: true });
-}
