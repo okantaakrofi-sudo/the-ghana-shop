ca53f064b199537eb85805b20b0f4f32376f5b38
okantaakrofi-sudo <okantaakrofi@gmail.com>
2026-04-05 07:43:03 +0400

    Update AnnouncementBar.tsx

--- components/AnnouncementBar.tsx
+++ components/AnnouncementBar.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 export default function AnnouncementBar() {
   return (
     <div className="bg-[#333] text-white text-[10px] py-2 text-center uppercase tracking-[0.2em] font-bold">
