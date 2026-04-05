6d70217e1a5ee308b4c65ecf01c810c041e252f4
okantaakrofi-sudo <okantaakrofi@gmail.com>
2026-04-05 05:14:31 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import AnnouncementBar from "@/components/AnnouncementBar";
+import WhatsAppButton from "@/components/WhatsAppButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
