e26172066345a14e6f0b624f3bcb797735680f98
okantaakrofi-sudo <okantaakrofi@gmail.com>
2026-04-05 10:06:17 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "../globals.css";
+import "./globals.css";
 import AnnouncementBar from "../components/AnnouncementBar";
 import WhatsAppButton from "../components/WhatsAppButton";
 
